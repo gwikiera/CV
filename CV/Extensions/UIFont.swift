5d82faf9045968ec06cda549af15afef6e0c1b89
Grzegorz Wikiera <gwikiera@gmail.com>
2020-08-04 23:49:40 +0200

    Fix open sans issue

--- CV/Extensions/UIFont.swift
+++ CV/Extensions/UIFont.swift
@@ -19,5 +19,5 @@ import UIKit
 
 extension UIFont {
     static var header: UIFont? { UIFont(name: "BebasNeue", size: 40) }
-    static var paragraph: UIFont? { UIFont(name: "OpenSans-Regular", size: 12) }
+    static var paragraph: UIFont? { UIFont(name: "OpenSans", size: 12) }
 }
