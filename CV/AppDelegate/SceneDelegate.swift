19afd604e2a8e53258772a7403e4314938ae35f9
Grzegorz Wikiera <gwikiera@gmail.com>
2020-08-22 07:34:00 +0200

    Main Storyboard removed

--- CV/AppDelegate/SceneDelegate.swift
+++ CV/AppDelegate/SceneDelegate.swift
@@ -22,10 +22,15 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
     var window: UIWindow?
 
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
-        // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
-        // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
-        // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
-        guard (scene as? UIWindowScene) != nil else { return }
+        guard let scene = scene as? UIWindowScene else { return }
+
+        window = UIWindow(windowScene: scene)
+        #if DEBUG
+        guard !isRunningUnitTests else { return }
+        window?.rootViewController = ViewController()
+        #endif
+
+        window?.makeKeyAndVisible()
     }
 
     func sceneDidDisconnect(_ scene: UIScene) {
