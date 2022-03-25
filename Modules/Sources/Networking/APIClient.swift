d911e413e97aa90a731dc0e8f5c3dbb3572e1883
Grzegorz Wikiera <grzegorz.wikiera@vodafone.com>
2022-03-25 20:09:38 +0100

    Fix imports in Modules

--- Modules/Sources/Networking/APIClient.swift
+++ Modules/Sources/Networking/APIClient.swift
@@ -15,7 +15,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-import UIKit
+import Foundation
 import Data
 import Combine
 import Common
