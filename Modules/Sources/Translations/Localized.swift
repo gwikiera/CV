15f19f9d1bd6cc19902b62736ddfa57b22336c1f
Grzegorz Wikiera <grzegorz.wikiera@vodafone.com>
2022-03-27 23:57:51 +0200

    `Localizable.strings` file extracted to the new `Translations` module

---
+++ Modules/Sources/Translations/Localized.swift
@@ -0,0 +1,7 @@
+import Foundation
+
+public extension String {
+    enum Localized {
+        public static let about = NSLocalizedString("about.header", bundle: .module, comment: "Header text for about section cell.")
+    }
+}
